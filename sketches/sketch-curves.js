c8f453cc9b5fc4c582932392bcef8a2c95ba6432
Agostina Tomalino <agostomalino@gmail.com>
2023-04-09 23:12:35 -0300

    curves divided into segments and break the grid

--- sketches/sketch-curves.js
+++ sketches/sketch-curves.js
@@ -1,93 +1,141 @@
 const canvasSketch = require('canvas-sketch');
-const random = require('canvas-sketch-util/random')
+const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
+const colormap = require('colormap');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+	dimensions: [ 1080, 1920 ],
+	animate: true,
 };
-//////////
-const sketch = ({width, height}) => {
-  const cols = 12;
-  const rows = 6;
-  const numCells = cols * rows;
 
-  //grid
-  const gw = width * 0.8;
-  const gh = height * 0.8;
-  // cell
-  const cw = gw / cols;
-  const ch = gh / rows;
-  //margins
-  const mx = (width - gw) * 0.5;
-  const my = (height - gh) * 0.5;
+const sketch = ({ width, height }) => {
+	const cols = 288;
+	const rows = 188;
+	const numCells = cols * rows;
 
-  const points = [];
-  let x, y, n;
-  let frequency = 0.002;
-  let amplitude = 90;
+	// grid
+	const gw = width  * 0.8;
+	const gh = height * 0.8;
+	// cell
+	const cw = gw / cols;
+	const ch = gh / rows;
+	// margin
+  const mx = (width  - gw) * 0.5;
+	const my = (height - gh) * 0.5;
 
-  
-  for(let i = 0; i < numCells; i++){
-    x = (i % cols) * cw;
-    y = Math.floor(i / cols) * ch;
+	const points = [];
 
-    n = random.noise2D(x, y, frequency, amplitude);
-    x += n;
-    y += n;    
-    points.push(new Point({x, y}));
-  }
+	let x, y, n, lineWidth, color;
+	let frequency = 0.002;
+	let amplitude = 90;
 
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+	const colors = colormap({
+		colormap: 'freesurface-blue',
+		nshades: amplitude,
+	});
 
-    context.save();
-    context.translate(mx, my);
-    context.translate(cw * 0.5, ch * 0.5);
-    context.strokeStyle = 'blue';
-    context.lineWidth = 4;
+	for (let i = 0; i < numCells; i++) {
+		x = (i % cols) * cw;
+		y = Math.floor(i / cols) * ch;
 
-    //draw lines
-    for(let r = 0; r < rows; r++){
-      context.beginPath();
-      for(c = 0; c < cols -1; c++){
-        const curr = points[r * cols + c + 0];
-        const next = points[r * cols + c + 1];
-        
-        const mx = curr.x + (next.x - curr.x) * 0.5;
-        const my = curr.y + (next.y - curr.y) * 0.5;
+		n = random.noise2D(x, y, frequency, amplitude);
+		// x += n;
+		// y += n;
 
-        if(c == 0) context.moveTo(curr.x, curr.y);
-        else if (c == cols - 2) context.quadraticCurveTo(curr.x, curr.y, next.x, next.y);
-        else context.quadraticCurveTo(curr.x, curr.y, mx, my);
-      }
-      context.stroke();
-    }
-    
-    points.forEach(point =>{
-      point.draw(context);
-    })
+		lineWidth = math.mapRange(n, -amplitude, amplitude, 0, 5);
 
-    context.restore();
-  };
+		color = colors[Math.floor(math.mapRange(n, -amplitude, amplitude, 0, amplitude))];
+
+		points.push(new Point({ x, y, lineWidth, color }));
+	}
+
+
+	return ({ context, width, height, frame }) => {
+		context.fillStyle = 'white';
+		context.fillRect(0, 0, width, height);
+
+		context.save();
+		context.translate(mx, my);
+		context.translate(cw * 0.5, ch * 0.5);
+		context.strokeStyle = 'blue';
+		context.lineWidth = 4;
+
+		// update positions
+		points.forEach(point => {
+			n = random.noise2D(point.ix + frame * 3, point.iy, frequency, amplitude);
+			point.x = point.ix + n;
+			point.y = point.iy + n;
+		});
+
+		let lastx, lasty;
+
+		// draw lines
+		for (let r = 0; r < rows; r++) {
+
+			for (let c = 0; c < cols - 1; c++) {
+				const curr = points[r * cols + c + 0];
+				const next = points[r * cols + c + 1];
+
+				const mx = curr.x + (next.x - curr.x) * 0.8;
+				const my = curr.y + (next.y - curr.y) * 0.5;
+
+				if (!c) {
+					lastx = curr.x;
+					lasty = curr.y;
+				}
+			
+				context.beginPath();
+				context.lineWidth = curr.lineWidth;
+				context.strokeStyle = curr.color;
+
+				context.moveTo(lastx, lasty);
+				context.quadraticCurveTo(curr.x, curr.y, mx, my);
+				
+				context.stroke();
+
+				// lastx = mx - c / cols * 250;
+				lasty = my;
+        lastx = mx;
+        // lasty = my;
+			}
+
+		}
+
+		// draw points
+		points.forEach(point => {
+			// point.draw(context);
+		});
+
+		context.restore();
+
+	};
 };
 
 canvasSketch(sketch, settings);
-////////
+
+
+
 class Point {
-  constructor({x, y, control}){
-    this.x = x;
-    this.y = y;
-  }
+	constructor({ x, y, lineWidth, color }) {
+		this.x = x;
+		this.y = y;
+		this.lineWidth = lineWidth;
+		this.color = color;
 
-  draw(context){
-    context.save();
-    context.translate(this.x, this.y);
-    context.fillStyle = 'blue';
+		this.ix = x;
+		this.iy = y;
+	}
 
-    context.beginPath();
-    context.arc(0, 0, 10, 0, Math.PI * 2);
-    context.fill();
-    context.restore();
-  };
+	draw(context) {
+		context.save();
+		context.translate(this.x, this.y);
+		context.fillStyle = 'blue';
 
+		context.beginPath();
+		context.arc(0, 0, 10, 0, Math.PI * 2);
+		context.fill();
+
+		context.restore();
+	}
 }
+
